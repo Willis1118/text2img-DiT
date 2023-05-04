948238421cc4b169c36cb922d4d9de45f9296a76
Willis1118 <nm3607@nyu.edu>
2023-05-04 19:56:08 -0400

    minor fic

--- tpu_train.py
+++ tpu_train.py
@@ -246,7 +246,7 @@ def main(args):
     logger.info(f"Dataset contains {len(train_dataset):,} images ({args.data_path})")
 
     # Prepare models for training:
-    update_ema(ema, model.module, decay=0)  # Ensure EMA is initialized with synced weights
+    update_ema(ema, model, decay=0)  # Ensure EMA is initialized with synced weights
     model.train()  # important! This enables embedding dropout for classifier-free guidance
     ema.eval()  # EMA model should always be in eval mode
 
