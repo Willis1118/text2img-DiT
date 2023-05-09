74604af9e76723f5f8c5479eb6147838a4857748
Willis1118 <nm3607@nyu.edu>
2023-05-09 10:12:27 -0400

    update

--- test.py
+++ test.py
@@ -93,7 +93,7 @@ def main():
         device,
     )
 
-    xm.rendezvous('finish loading')
+    # xm.rendezvous('finish loading')
 
     print(f"Dataset contains {len(train_dataset):,} images ({data_path})")
     
